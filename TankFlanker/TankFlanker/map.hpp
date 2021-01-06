e9ad91d035403466a3069f1463f5e4d4e9fa7091
ubw emiya <a6m3.zero.f@gmail.com>
2021-01-06 14:48:40 +0900

    ss

--- TankFlanker/TankFlanker/map.hpp
+++ TankFlanker/TankFlanker/map.hpp
@@ -2,7 +2,7 @@
 
 // プレイヤー関係の定義
 #define PLAYER_ENUM_DEFAULT_SIZE	1.8f	// 周囲のポリゴン検出に使用する球の初期サイズ
-#define PLAYER_HIT_WIDTH			0.25f	// 当たり判定カプセルの半径
+#define PLAYER_HIT_WIDTH			0.5f	// 当たり判定カプセルの半径
 #define PLAYER_HIT_HEIGHT			1.8f	// 当たり判定カプセルの高さ
 #define PLAYER_HIT_TRYNUM			16		// 壁押し出し処理の最大試行回数
 #define PLAYER_HIT_SLIDE_LENGTH		0.05f	// 一度の壁押し出し処理でスライドさせる距離
